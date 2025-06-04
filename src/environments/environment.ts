5fa29a6b5034fc6206f1199034b740d7c5c7ed8d
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2025-06-04 20:09:46 -0300

    - incluindo environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        "projectId": "device-management-hub",
+        "appId": "1:550722231398:web:6a767c1568b0906959a1a7",
+        "storageBucket": "device-management-hub.firebasestorage.app",
+        "apiKey": "AIzaSyCHJ3-DX1sVnwQCf-oXlADuIwpFijIm3Kw",
+        "authDomain": "device-management-hub.firebaseapp.com",
+        "messagingSenderId": "550722231398",
+        "measurementId": "G-6RN5FHS5M4"
+    }
+}
